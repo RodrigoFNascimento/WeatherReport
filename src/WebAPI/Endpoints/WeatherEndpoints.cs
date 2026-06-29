dcb4a1dda73b49e3d965e86dc4369b0ee57e45dc
Rodrigo Nascimento <rodrigofn0@hotmail.com>
2026-06-29 18:03:47 -0300

    Move weather forecast endpoint to /Endpoints

---
+++ src/WebAPI/Endpoints/WeatherEndpoints.cs
@@ -0,0 +1,33 @@
+﻿namespace WebAPI.Endpoints;
+
+internal static class WeatherEndpoints
+{
+    public static IEndpointRouteBuilder MapWeatherEndpoints(this IEndpointRouteBuilder endpoints)
+    {
+        var summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        endpoints.MapGet("/weatherforecast", () =>
+        {
+            var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+                .ToArray();
+            return forecast;
+        })
+        .WithName("GetWeatherForecast");
+
+        return endpoints;
+    }
+}
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
