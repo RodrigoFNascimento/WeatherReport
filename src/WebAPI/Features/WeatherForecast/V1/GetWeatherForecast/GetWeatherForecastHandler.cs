b98ab113cb2dc0c593eb7182a63de2e3d70fd9fa
Rodrigo Nascimento <rodrigofn0@hotmail.com>
2026-07-13 16:44:13 -0300

    Add documentation

--- src/WebAPI/Features/WeatherForecast/V1/GetWeatherForecast/GetWeatherForecastHandler.cs
+++ src/WebAPI/Features/WeatherForecast/V1/GetWeatherForecast/GetWeatherForecastHandler.cs
@@ -9,6 +9,8 @@ internal static class GetWeatherForecastHandler
     /// <summary>
     /// Handles the logic for the endpoint that gets the weather forecast.
     /// </summary>
+    /// <param name="sender">Sends a mediator request.</param>
+    /// <param name="httpResultMapper">Maps a result to an HTTP response.</param>
     /// <returns>The endpoint result.</returns>
     public static async Task<IResult> Handle(ISender sender, IHttpResultMapper httpResultMapper)
     {
