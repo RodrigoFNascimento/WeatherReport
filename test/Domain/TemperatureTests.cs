017e92c68c5883ae999574f2092c7e9f77e5a4e1
Rodrigo Nascimento <rodrigofn0@hotmail.com>
2026-08-16 11:31:28 -0300

    Implement Domain unit tests

---
+++ test/Domain/TemperatureTests.cs
@@ -0,0 +1,29 @@
+﻿namespace Domain.Tests.Unit;
+
+public sealed class TemperatureTests
+{
+    [Fact]
+    public void FromCelsius_WhenValidCelsiusDegrees_ShouldReturnTemperature()
+    {
+        // Arrange
+        const double MinimumCelsiusTemperature = -273.15;
+        var celsiusDegrees = MinimumCelsiusTemperature + 0.01;
+
+        // Act
+        var result = Temperature.FromCelsius(celsiusDegrees);
+
+        // Assert
+        Assert.Equal(celsiusDegrees, result.DegreesCelsius);
+    }
+    
+    [Fact]
+    public void FromCelsius_WhenInvalidCelsiusDegrees_ShouldThrowArgumentOutOfRangeException()
+    {
+        // Arrange
+        const double MinimumCelsiusTemperature = -273.15;
+        var celsiusDegrees = MinimumCelsiusTemperature - 0.01;
+
+        // Act & Assert
+        Assert.Throws<ArgumentOutOfRangeException>(() => Temperature.FromCelsius(celsiusDegrees));
+    }
+}
